bb5d081d90882be182317d2cf3a33742dcd1e216
Matt Koski <maccam912@gmail.com>
2024-06-01 08:44:24 -0500

    waveform works

--- src/app.rs
+++ src/app.rs
@@ -1,109 +1,137 @@
-/// We derive Deserialize/Serialize so we can persist app state on shutdown.
-#[derive(serde::Deserialize, serde::Serialize)]
-#[serde(default)] // if we add new fields, give them default values when deserializing old state
-pub struct TemplateApp {
-    // Example stuff:
-    label: String,
+use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
+use cpal::{Device, Stream, StreamConfig};
+use egui::{Color32, Pos2, Rect, Vec2};
+use std::sync::{Arc, Mutex};
 
-    #[serde(skip)] // This how you opt-out of serialization of a field
+#[derive(serde::Deserialize, serde::Serialize)]
+#[serde(default)]
+pub struct Js8App {
+    label: String,
+    #[serde(skip)]
     value: f32,
+    #[serde(skip)]
+    audio_data: Arc<Mutex<Vec<f32>>>,
+    #[serde(skip)]
+    stream: Option<Stream>,
+    #[serde(skip)]
+    devices: Vec<Device>,
+    #[serde(skip)]
+    selected_device_index: usize,
 }
 
-impl Default for TemplateApp {
+impl Default for Js8App {
     fn default() -> Self {
+        let host = cpal::default_host();
+        let devices: Vec<Device> = host.input_devices().unwrap().collect();
         Self {
-            // Example stuff:
             label: "Hello World!".to_owned(),
             value: 2.7,
+            audio_data: Arc::new(Mutex::new(vec![])),
+            stream: None,
+            devices,
+            selected_device_index: 0,
         }
     }
 }
 
-impl TemplateApp {
-    /// Called once before the first frame.
+impl Js8App {
     pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
-        // This is also where you can customize the look and feel of egui using
-        // `cc.egui_ctx.set_visuals` and `cc.egui_ctx.set_fonts`.
-
-        // Load previous app state (if any).
-        // Note that you must enable the `persistence` feature for this to work.
         if let Some(storage) = cc.storage {
             return eframe::get_value(storage, eframe::APP_KEY).unwrap_or_default();
         }
-
         Default::default()
+    }
+
+    fn start_audio_stream(&mut self) {
+        let device = &self.devices[self.selected_device_index];
+        let config = device.default_input_config().unwrap();
+
+        let audio_data = self.audio_data.clone();
+
+        let stream = device.build_input_stream(
+            &config.into(),
+            move |data: &[f32], _: &cpal::InputCallbackInfo| {
+                if !data.is_empty() {
+                    let mut audio_data = audio_data.lock().unwrap();
+                    audio_data.extend_from_slice(data);
+                    if audio_data.len() > 1024 * 1024 {
+                        audio_data.drain(..data.len());
+                    }
+                }
+            },
+            move |err| {
+                eprintln!("Stream error: {}", err);
+            },
+            None,
+        ).unwrap();
+
+        stream.play().unwrap();
+        self.stream = Some(stream);
+    }
+
+    fn draw_waterfall(&self, ui: &mut egui::Ui) {
+        let audio_data = self.audio_data.lock().unwrap();
+
+        let width = ui.available_width();
+        let height = ui.available_height();
+        let rect = ui.available_rect_before_wrap();
+
+        ui.painter().rect_filled(rect, 0.0, Color32::BLACK);
+
+        let sample_rate = 44100; // Assuming a sample rate of 44.1 kHz
+        let num_samples = std::cmp::min(audio_data.len(), sample_rate); // Limit to the last second of data
+
+        if num_samples > 0 {
+            let step = width / num_samples as f32;
+            let start_index = audio_data.len().saturating_sub(sample_rate);
+            for (i, &sample) in audio_data[start_index..].iter().enumerate() {
+                let x = rect.min.x + i as f32 * step;
+                let y = rect.min.y + height / 2.0 - sample * height / 2.0;
+                ui.painter().line_segment(
+                    [Pos2::new(x, rect.min.y + height / 2.0), Pos2::new(x, y)],
+                    (1.0, Color32::WHITE),
+                );
+            }
+        }
     }
 }
 
-impl eframe::App for TemplateApp {
-    /// Called by the frame work to save state before shutdown.
+impl eframe::App for Js8App {
     fn save(&mut self, storage: &mut dyn eframe::Storage) {
         eframe::set_value(storage, eframe::APP_KEY, self);
     }
 
-    /// Called each time the UI needs repainting, which may be many times per second.
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        // Put your widgets into a `SidePanel`, `TopBottomPanel`, `CentralPanel`, `Window` or `Area`.
-        // For inspiration and more examples, go to https://emilk.github.io/egui
+        // Request a repaint for continuous updates
+        ctx.request_repaint();
 
         egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
-            // The top panel is often a good place for a menu bar:
-
             egui::menu::bar(ui, |ui| {
-                // NOTE: no File->Quit on web pages!
-                let is_web = cfg!(target_arch = "wasm32");
-                if !is_web {
-                    ui.menu_button("File", |ui| {
-                        if ui.button("Quit").clicked() {
-                            ctx.send_viewport_cmd(egui::ViewportCommand::Close);
-                        }
-                    });
-                    ui.add_space(16.0);
+                if ui.button("Start Audio Stream").clicked() {
+                    self.start_audio_stream();
                 }
-
+                ui.add_space(16.0);
                 egui::widgets::global_dark_light_mode_buttons(ui);
             });
         });
 
         egui::CentralPanel::default().show(ctx, |ui| {
-            // The central panel the region left after adding TopPanel's and SidePanel's
-            ui.heading("eframe template");
+            ui.heading("Audio Waterfall Visualization");
 
-            ui.horizontal(|ui| {
-                ui.label("Write something: ");
-                ui.text_edit_singleline(&mut self.label);
-            });
+            // Dropdown for selecting the input device
+            egui::ComboBox::from_label("Select Input Device")
+                .selected_text(format!("{}", self.devices[self.selected_device_index].name().unwrap()))
+                .show_ui(ui, |ui| {
+                    for (index, device) in self.devices.iter().enumerate() {
+                        ui.selectable_value(&mut self.selected_device_index, index, device.name().unwrap());
+                    }
+                });
 
-            ui.add(egui::Slider::new(&mut self.value, 0.0..=10.0).text("value"));
-            if ui.button("Increment").clicked() {
-                self.value += 1.0;
-            }
+            // Add some space between the dropdown and the waterfall
+            ui.add_space(16.0);
 
-            ui.separator();
-
-            ui.add(egui::github_link_file!(
-                "https://github.com/emilk/eframe_template/blob/main/",
-                "Source code."
-            ));
-
-            ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
-                powered_by_egui_and_eframe(ui);
-                egui::warn_if_debug_build(ui);
-            });
+            // Draw the waterfall visualization
+            self.draw_waterfall(ui);
         });
     }
-}
-
-fn powered_by_egui_and_eframe(ui: &mut egui::Ui) {
-    ui.horizontal(|ui| {
-        ui.spacing_mut().item_spacing.x = 0.0;
-        ui.label("Powered by ");
-        ui.hyperlink_to("egui", "https://github.com/emilk/egui");
-        ui.label(" and ");
-        ui.hyperlink_to(
-            "eframe",
-            "https://github.com/emilk/egui/tree/master/crates/eframe",
-        );
-        ui.label(".");
-    });
 }
